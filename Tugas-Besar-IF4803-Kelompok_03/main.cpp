f10e67af87d0359c12b8cc50b8bed5a37e0fa8b2
Naufal Nabhani Augusta <naufalnbn@gmail.com>
2025-12-11 15:21:36 +0700

    menambahkan main.cpp

---
+++ Tugas-Besar-IF4803-Kelompok_03/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "main.h"
+
+using namespace std;
+
+int main()
+{
+    int option=-99;
+    while (option != 0) {
+        system("cls");
+        cout << "============ Menu ============ " << endl;
+        cout << "|| 1. Test Case             ||" << endl;
+        cout << "|| 2. Study Case            ||" << endl;
+        cout << "|| 0. exit                  ||" << endl;
+        cout << "============================== " << endl;
+        cout << "Choose your option : ";
+        cin >> option;
+        switch(option) {
+           case 1  :
+              cout << "you choose option 1" << endl;
+              break;
+           case 2  :
+              cout << "you choose option 2" << endl;
+              break;
+
+           default :
+              exit(1);
+        }
+    }
+
+
+    return 0;
+}
