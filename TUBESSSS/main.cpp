a20dd34a58efceaf36ac4ad24ce450cc9668e685
IRONDHIF <abdan.nadhif@gmail.com>
2025-12-12 14:52:49 +0700

    Initial Commit

---
+++ TUBESSSS/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TUBESSSS
+//
+//  Created by Abdan Nadhif on 12/12/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
