1b9c0d856530d03d95fdf3c74ebe3164d554eaa9
Naufal Nabhani Augusta <naufalnbn@gmail.com>
2025-12-14 00:11:56 +0700

    Update guru.h
    Mengubah parameter dari findElemenGuru

--- Tugas-Besar-IF4803-Kelompok_03/guru.h
+++ Tugas-Besar-IF4803-Kelompok_03/guru.h
@@ -11,7 +11,7 @@ typedef struct elmenGuru *adrP;
 
 struct guru{
     string nama;
-    int ID;
+    string kode;
     int umur;
 };
 
@@ -26,15 +26,15 @@ struct ListGuru{
 };
 
 void createListGuru(ListGuru &L);
-adrP createElementGuru(infotypeP x);
+adrP createElementGuru(string nama, string kode, int umur);
 void insertFirsttGuru(ListGuru &L, adrP P);
 void insertLastGuru(ListGuru &L, adrP p);
 void insertAfterGuru(ListGuru &L, adrP p, adrP prec);
 void deleteFirstGuru(ListGuru &L, adrP &p);
 void deleteLastGuru(ListGuru &L, adrP &p);
 void deleteAfterGuru(ListGuru &L, adrP &p, adrP prec);
-adrP findElemenGuru(ListGuru L, infotypeP x);
+adrP findElemenGuru(ListGuru L, string nama);
 void viewParent(ListGuru L);
-void mennghitungJumlahGuru(ListGuru L);
+void menghitungJumlahGuru(ListGuru L);
 
 #endif // GURU_H_INCLUDED
